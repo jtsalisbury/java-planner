4d7c5c78e712621bf2ac0cddfbe287599c2d3b20
crazyscouter <crazyscouter1@gmail.com>
2018-06-17 19:44:47 -0400

    Initial Commit

---
+++ src/mytasks/Main.java
@@ -0,0 +1,15 @@
+package mytasks;
+
+public class Main {
+	public static final String dataFileName = "mytasks_data.txt";
+	
+	static String workingDir;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		workingDir = System.getProperty("user.dir");
+	}
+	
+	
+}
