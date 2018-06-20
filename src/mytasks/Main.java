0b6ef9b4fc2e0d3e5b1f32c1b8e49f993d1ae868
crazyscouter <crazyscouter1@gmail.com>
2018-06-20 19:45:28 -0400

    Begin UI and actions for logging in

--- src/mytasks/Main.java
+++ src/mytasks/Main.java
@@ -1,14 +1,20 @@
 package mytasks;
 
-import java.io.File;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class Main {	
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
+public class Main extends Application {	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 		SQLManager db = new SQLManager();
+		UserManager user = new UserManager(primaryStage);
 		
+		user.promptLogin();
 		
+	}
+	public static void main(String[] args) {
+		launch(args);
 	}
 	
 }
--- src/mytasks/UserManager.java
+++ src/mytasks/UserManager.java
@@ -1,5 +1,26 @@
 package mytasks;
 
-public class UserManager {
+import java.io.IOException;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.*;
 
+public class UserManager {
+	private Stage primaryStage;
+	UserManager(Stage stg) {
+		primaryStage = stg;
+	}
+	
+	public void promptLogin() throws IOException {
+		
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		Parent p = FXMLLoader.load(getClass().getResource("loginForm.fxml"));
+		
+		Scene s = new Scene(p);
+		
+		//s.getStylesheets().add(getClass().getResource("loginForm.css").toExternalForm());
+		
+		primaryStage.setScene(s);
+		primaryStage.show();
+	}
 }
